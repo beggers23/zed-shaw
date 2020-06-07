5dc1bbe431f1714a6ba3fac2526ae0ab1895895f
Brendan <eggers.brendan@gmail.com>
2020-06-07 09:38:21 -0400

    Keeping some notes, starting ex 23

---
+++ notes.py
@@ -0,0 +1,8 @@
+# Need to keep some notes handy
+
+in_file = "start.txt"
+out_file = "sample.txt"
+# reads and assigns variable with file data
+read_file = open(in_file).read()
+# writes variable into new file
+write_file = open(out_file, "w").write(read_file)
