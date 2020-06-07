73b97c85335d489eada6fe2932a14de0e0e55bbc
Brendan <eggers.brendan@gmail.com>
2020-06-06 22:48:50 -0400

    Copying over for better repo

---
+++ app.py
@@ -0,0 +1,18 @@
+from sys import argv
+from os.path import exists
+# script is the file being called
+# each one after is the available arguement passed
+# script, butts, second, third = argv
+# You can redefine first to butts and have that variable defined
+# Open uses a string to find the filename. Can hard code extension
+# txt = open(f"{filename}.txt")
+# print(txt.read())
+
+# target = open(filename, "w")
+# "w" = write "r" = read "a" = append
+# target.truncate()
+# target.write("text")
+# target.close()
+# last line finishes it
+# Zed Shaws - Learn Python 3 the Hard Way
+# page 56
